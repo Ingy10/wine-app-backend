0396a62558a0711a99c02000020ff59eeaddf96a
Kyle Ingham <kyleingy@proton.me>
2025-02-27 13:55:55 -0700

    Project Initialization

---
+++ src/main/java/com/wineapp/wine_app_backend/WineAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wineapp.wine_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wineapp/wine_app_backend/WineAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wineapp.wine_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
